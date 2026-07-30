d923f07fedef0ed8472b0a2843c71cfb4d5f0f32
ManaMojiNeo <supermojineo@gmail.com>
2026-07-30 09:55:24 +0700

    Add NavBar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <div className="navbar">
+      <div className="nav-inner">
+        <Link href="/">
+          <div>
+            <span className="logo-text">คั่วบ้าน</span>
+            <span className="logo-sub">คั่วด้วยใจ</span>
+          </div>
+        </Link>
+        <div className="nav-links">
+          <Link href="/menu">เมนู</Link>
+          <Link href="/requests">เสนอเมนู</Link>
+          <Link href="/login" className="btn btn-primary">
+            เข้าสู่ระบบ
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
